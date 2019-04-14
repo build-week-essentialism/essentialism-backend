d732d3de8616c58320cacf5d0acd02e4106265c3
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-14 12:50:26 -0400

    Wrote authModel and started writing tests for authModel.

--- models/authModel.spec.js
+++ models/authModel.spec.js
@@ -3,25 +3,58 @@ const db = require('../utilities/dbConfig');
 const Auth = require('./authModel');
 
 describe('authModel', () => {
+  afterEach(async () => {
+    await db('users').truncate();
+  });
+
   describe('getUser()', () => {
     test('should return falsy if username does not exist', async () => {
       const unique = await Auth.getUser({
         username: 'hickorydickorydock'
       });
-      console.log(unique);
       expect(unique).toBeFalsy();
     });
 
-    test('should return user if username does exist', async () => {
-      const unique = await Auth.getUser({ username: 'john' });
-      console.log(unique);
+    test('should return user if username exists', async () => {
+      await Auth.registerUser({
+        username: 'billie',
+        password: '123',
+        email: 'billieeilish@gmail.com',
+        firstName: 'Billie',
+        lastName: 'Eilish'
+      });
+      const unique = await Auth.getUser({ username: 'billie' });
       expect(unique).toBeTruthy();
+      expect(unique.username).toBe('billie');
     });
   });
 
-  describe('registerUser()', () => {
-    //   beforeEach(async () => {
-    //     await db('users').truncate();
-    //   });
-  });
+  //   describe('registerUser()', () => {
+  //     test('should return user if proper request', async () => {
+  //         await Auth.registerUser({
+  //             username: 'billie',
+  //             password: '123',
+  //             email: 'billieeilish@gmail.com',
+  //             firstName: 'Billie',
+  //             lastName: 'Eilish'
+  //           });
+  //       const unique = await Auth.getUser({
+  //         username: 'hickorydickorydock'
+  //       });
+  //       expect(unique).toBeFalsy();
+  //     });
+
+  //     test('should return user if username exists', async () => {
+  //       await Auth.registerUser({
+  //         username: 'billie',
+  //         password: '123',
+  //         email: 'billieeilish@gmail.com',
+  //         firstName: 'Billie',
+  //         lastName: 'Eilish'
+  //       });
+  //       const unique = await Auth.getUser({ username: 'billie' });
+  //       expect(unique).toBeTruthy();
+  //       expect(unique.username).toBe('billie');
+  //     });
+  //   });
 });
