281f511a3e70f0728cf7772bb6806d9953f9fe9e
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-04-15 15:17:26 +0200

    GET and POST method for projects

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const server = express();
 const authRouter = require('../routers/authRouter');
 const createdValuesRouter = require('../routers/createdValuesRouter');
 const defaultValuesRouter = require('../routers/defaultValuesRouter');
-// const projectsRouter = require('../routers/projectsRouter');
+const projectsRouter = require('../routers/projectsRouter');
 // const userValuesRouter = require('../routers/userValuesRouter');
 
 server.use(helmet());
@@ -17,7 +17,7 @@ server.use(cors());
 server.use('/api/auth', authRouter);
 server.use('/api/createdvalues', createdValuesRouter);
 server.use('/api/defaultvalues', defaultValuesRouter);
-// server.use('/api/projects', projectsRouter);
+server.use('/api/projects', projectsRouter);
 // server.use('/api/uservalues', userValuesRouter);
 
 server.get('/', (req, res) => {
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -13,8 +13,52 @@ const error500 = {
   message: "Something went wrong when getting your request. Make sure the request is foolproof"
 }
 
-router.get('/', (req, res) => {
+// Project object
+// {
+//   "id": 1,
+//   "user_id": 1,  <-- REFERENCES ID IN USERS (FOREIGN KEY)
+//   "project_name": "taking a class",
+//   "project_active": 1  <-- IS A BOOLEAN. DEFAULT = 1 (TRUE)
+// }
 
+// GET ALL PROJECTS LINKED TO A USER. ID REFERS TO USER_ID, WHICH IS LINKED TO ID IN USERS
+router.get('/:id', (req, res) => {
+  const user_id = req.params
+  if (!user_id) {
+    res.status(404).json(error404)
+  } else {
+    projects.getProjects(user_id.id)
+      .then(data => {
+        res.status(200).json(data)
+      })
+      .catch(() => {
+        res.status(500).json(error500)
+      })
+  }
+})
+
+router.post('/', (req, res) => {
+  const project = req.body
+  if (!project.user_id || !project.project_name) {
+    res.status(404).json({ message: "Please make sure you pass both a 'user_id' and a 'project_name'"})
+  } else {
+    projects.addProject(project)
+      .then(data => {
+        // RETURNS NEWLY-MADE PROJECT OBJECT
+        res.status(201).json(data)
+      })
+      .catch(() => {
+        res.status(500).json(error500)
+      })
+  }
+})
+
+router.put('/', (req, res) => {
+  
+})
+
+router.delete('/', (req, res) => {
+  
 })
 
 module.exports = router;
