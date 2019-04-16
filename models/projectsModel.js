64a7f72b9ce3080823bf0ec2c2d4f666d260c564
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-16 17:13:12 -0400

    Refactored createdValuesModel.js to return a created values array for each database call. Rewrote some comments to be clearer. Currently fails 4 tests, which I will work on next.

--- models/projectsModel.js
+++ models/projectsModel.js
@@ -5,13 +5,14 @@ const getProjects = async user_id => {
   return await db('projects').where({ user_id });
 };
 
+// The user_id needed to return the projects array is included in the project parameter
 const addProject = async project => {
   await db('projects').insert(project);
   return getProjects(project.user_id);
 };
 
+// The user_id parameter is used to return the projects array after the update
 // The id parameter is the project id (primary key)
-// The user_id parameter is used to return the array of projects after the update
 const updateProject = async (user_id, id, updates) => {
   await db('projects')
     .where({ id })
@@ -19,6 +20,7 @@ const updateProject = async (user_id, id, updates) => {
   return getProjects(user_id);
 };
 
+// The user_id parameter is used to return the projects array after the delete
 // The id parameter is the project id (primary key)
 const deleteProject = async (user_id, id) => {
   await db('projects')
